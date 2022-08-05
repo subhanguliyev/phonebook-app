51febb37112fa9e309b866b1f71a27a3e3ae8277
Subhan <squadiloglu@gmail.com>
2022-08-05 21:14:30 +0400

    5 passed in 0.80s

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,31 @@ from app import *
 def test_insert_persons():
     name = 'Coco'
     number = 654
-    insert = f"""
-    INSERT INTO phonebook.phonebook (name, number)
-    VALUES ('{name.strip().lower()}', '{number}');
-    """
     result = insert_person(name, number)
-
     assert result == 'Person with name Coco already exits.'
+
+
+def test_find_persons_yes():
+    keyword = 'Bill'
+    person = find_persons(keyword)
+    assert person == [{'id': 6, 'name': 'Bill', 'number': '987'}]
+
+
+def test_find_persons_not():
+    keyword = 'Obama'
+    person = find_persons(keyword)
+    assert person == [{'name': 'No Result', 'number': 'No Result'}]
+
+
+def test_update_person():
+    name = 'Marina'
+    number = 222
+    result = update_person(name, number)
+    assert result == f'Person with name {name.strip().title()} does not exist.'
+
+
+def test_delete_person():
+    name = 'Sara'
+    result = delete_person(name)
+    assert result == f'Phone record of {name.strip().title()} is deleted from the phonebook successfully'
 
